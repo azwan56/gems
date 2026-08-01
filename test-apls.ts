0ca1e1867f768a67a7fa768b06894682e9e0278a
Alex Wan <azwan56@gmail.com>
2026-08-01 20:59:15 +0800

    chore: remove test script

--- test-apls.ts
+++
@@ -1,9 +0,0 @@
-import { config } from "dotenv";
-config({ path: ".env.local" });
-import { fmpFetch } from "./src/lib/fmp-fetch";
-async function main() {
-  const profile = await fmpFetch<any[]>("/profile", { symbol: "APLS" });
-  console.log(profile);
-  process.exit(0);
-}
-main();
